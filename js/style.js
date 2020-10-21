079c7ee576cafd4b05643a2c9423ac71d5dd437a
Ereke013 <51377709+Ereke013@users.noreply.github.com>
2020-10-21 23:14:35 +0600

    reapair design, add animation

--- js/style.js
+++ js/style.js
@@ -1,4 +1,11 @@
-window.addEventListener("scroll", function (){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY>0)
-})
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+    document.getElementById("myDiv2").style.display = "block";
+}
